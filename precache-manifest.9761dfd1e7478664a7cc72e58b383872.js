2fbd89c013c8a0c123863707d9013a254a55cf74
Peter Frohner <peter@begame.nl>
2018-10-03 09:38:55 +0200

    5844953

---
+++ precache-manifest.9761dfd1e7478664a7cc72e58b383872.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/vue-calculator/robots.txt"
+  },
+  {
+    "revision": "43c4224e8b154d49df61",
+    "url": "/vue-calculator/js/chunk-vendors.6d06cc1d.js"
+  },
+  {
+    "revision": "506e8a67e4effa50481e",
+    "url": "/vue-calculator/js/app.32f58e9c.js"
+  },
+  {
+    "revision": "fad572b4d095cb8ce01d9c0ecebee01d",
+    "url": "/vue-calculator/index.html"
+  },
+  {
+    "revision": "506e8a67e4effa50481e",
+    "url": "/vue-calculator/css/app.8aeb7e19.css"
+  }
+];
